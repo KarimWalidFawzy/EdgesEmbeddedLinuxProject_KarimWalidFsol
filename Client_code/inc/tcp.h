213ac8543ddcfd231aca3d5772f38642f78ad2e6
KarimWalidFawzy <kwasaq.0183@gmail.com>
2026-03-10 21:39:00 +0200

    Changes to be committed: 	modified:   Client_code/inc/tcp.h 	modified:   Client_code/inc/udp.h 	new file:   Yocto_files/Classes/kernel.bbclass

--- Client_code/inc/tcp.h
+++ Client_code/inc/tcp.h
@@ -1,6 +1,11 @@
 #ifndef TCP_H
 #define TCP_H
+    #include "socket.h"
     #if !__cplusplus 
     #error "This program requires C++"
     #endif
+    class TCPSocket : public Socket
+    {
+
+    };
 #endif 
---
+++ Client_code/inc/udp.h
@@ -0,0 +1,11 @@
+#ifndef UDP_H
+#define UDP_H
+    #include "socket.h"
+    #if !__cplusplus 
+    #error "This program requires C++"
+    #endif
+    class UDPSocket : public Socket
+    {
+        
+    };
+#endif 
